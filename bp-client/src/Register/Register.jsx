782077d076bd44bbb397b4580ee65aad2c625d3d
Alena Kamenská <alenka.kamenska@seznam.cz>
2026-04-04 11:06:12 +0200

    Register

--- bp-client/src/Register/Register.jsx
+++ bp-client/src/Register/Register.jsx
@@ -3,17 +3,18 @@ import "./Register.css";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom"; 
 import axios from "axios"; 
-import { useAuthContext, SET_ACCESS_TOKEN } from "../Providers/AuthProvider"; 
 import { Input } from "../components/Input/Input";
 import { Select } from "../components/Select/Select";
 import { Button } from "../components/Button/Button";
 import Error from "../components/Error/Error"
+import { toast } from 'react-toastify';
 
 export const Register = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, formState: { errors }, watch } = useForm();
     const navigate = useNavigate();
-    const [, dispatch] = useAuthContext();
     const [serverErrors, setServerErrors] = useState([]);
+    const API_BASE_URL = process.env.REACT_APP_API_URL;
+
     const roleOptions = [
         { value: "User", label: "Zákazník" },
         { value: "Business", label: "Zahradnictví/Květinářství" }
@@ -21,13 +22,14 @@ export const Register = () => {
 
     const onSubmit = data => {
         setServerErrors([]); 
-        axios.post("https://localhost:7014/api/Auth/Register", {
+        axios.post(`${API_BASE_URL}/Auth/Register`, {
             email: data.email, 
             password: data.password,
             role: data.role
         })
         .then(response => {
             navigate("/login"); 
+            toast.success("Profil byl úspěšně vytvořen");
         })
         .catch(error => {
             if (error.response && error.response.data) {
@@ -68,12 +70,39 @@ export const Register = () => {
                     })}
                     placeholder="zadejte heslo..."
                 />
+                <Input
+                    label="Heslo znovu"
+                    type="password"
+                    {...register("confirmPassword", { 
+                        validate: (val) => {
+                            if (watch('password') !== val) {
+                            return "Hesla se neshodují";
+                            }
+                        },
+                    })}
+                    error={errors.confirmPassword}
+                />
                 <Select
                     label="Role"
                     error={errors.role}
                     options={roleOptions}
                     {...register("role", { required: "Role je povinná" })}
                 />
+                <div className="gdpr-checkbox-container">
+                    <div className="checkbox-row">
+                        <input
+                            type="checkbox"
+                            id="gdpr"
+                            {...register("gdpr", { 
+                                required: "Pro registraci musíte souhlasit se zpracováním údajů" 
+                            })}
+                        />
+                        <label htmlFor="gdpr">
+                            Souhlasím se <a href="/ochrana-soukromi" target="_blank" rel="noopener noreferrer">zpracováním osobních údajů</a>
+                        </label>
+                    </div>
+                    {errors.gdpr && <span className="error-text">{errors.gdpr.message}</span>}
+                </div>
                  <Error serverErrors={serverErrors}/>
                 <Button variant="primary" type="submit" text="Registrovat se"/>
             </form>
